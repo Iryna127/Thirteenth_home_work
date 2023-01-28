b412947a1e59e645a6084aece11fa90f76264a99
Iryna127 <irusia_1111@ukr.net>
2023-01-28 16:27:28 +0200

    new

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// First task
+const greeting = new Promise((resolve, reject) =>
+  setTimeout(() => {
+    console.log('Hello');
+  }, 3000)
+);
+
+// Second task
+async function deleay() {
+  console.log('first call');
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), 4000);
+  });
+  let result = await promise;
+  result;
+  console.log('second call');
+}
+deleay();
+
+// Third task
+
+function createDice() {
+  let arrNew = [];
+  for (let i = 0; i < 3; i++) {
+    arrNew.push(
+      new Promise((resolve, reject) => {
+        let num = Math.round(Math.random() * 10);
+
+        if (num >= 6) {
+          resolve('Успех');
+          console.log(`Успех = ${num}`);
+        } else {
+          reject('Провал');
+          console.log(`Провал = ${num}`);
+        }
+      })
+    );
+  }
+  Promise.all(arrNew)
+    .then(() => {
+      console.log('You are lucky!');
+    })
+    .catch(() => {
+      console.log('Not today(');
+    });
+}
+createDice();
